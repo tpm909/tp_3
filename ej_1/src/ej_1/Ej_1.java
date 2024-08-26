8f91a2bfd9a8a3c80987ad26b85adab86ec6969b
tpm909 <ankitoperez909@gmail.com>
2024-08-26 16:12:48 -0300

    cosa
    cosa

--- ej_1/src/ej_1/Ej_1.java
+++ ej_1/src/ej_1/Ej_1.java
@@ -14,7 +14,7 @@ public class Ej_1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
     
 }
